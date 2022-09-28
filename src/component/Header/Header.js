3993b6660d8728d61be635ca6fa297f2f3142b15
Farjana Islam <farjanaanika2@gmail.com>
2022-09-29 01:38:32 +0600

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className='bg-primary'>Maintain A HeaLThy LifeStyle</h2>
+            <h3>Select today's routine</h3>
+        </div>
+    );
+};
+
+export default Header;
